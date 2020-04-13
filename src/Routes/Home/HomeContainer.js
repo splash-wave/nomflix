b650d9970b8167f4b8f070dc2a3b6bb293bec646
arimkim <arim721@gmail.com>
2020-04-13 17:32:08 +0900

    HomeContainer, TVContainer

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomePresenter from './HomePresenter';
+import { moviesApi } from 'api';
 
 export default class extends React.Component {
   state = {
@@ -10,8 +11,39 @@ export default class extends React.Component {
     loading: true,
   };
 
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: nowPlaying },
+      } = await moviesApi.nowPlaying();
+      // console.log(nowPlaying);
+      const {
+        data: { results: upcoming },
+      } = await moviesApi.upcoming();
+      // console.log(upcoming);
+      const {
+        data: { results: popular },
+      } = await moviesApi.popular();
+      // console.log(popular);
+      this.setState({
+        nowPlaying,
+        upcoming,
+        popular,
+      });
+    } catch {
+      this.setState({
+        error: "Can't find Movie information",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
+
   render() {
     const { nowPlaying, upcoming, popular, error, loading } = this.state;
+    console.log(this.state);
     return <HomePresenter nowPlaying={nowPlaying} upcoming={upcoming} popular={popular} error={error} loading={loading} />;
   }
 }
--- src/api.js
+++ src/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.themoviedb.org/3/'
+  baseURL: 'https://api.themoviedb.org/3/',
 });
 
-api.interceptors.request.use(config => {
+api.interceptors.request.use((config) => {
   config.params = config.params || {};
-  config.params['api_key'] = "3b9c855ca7a0d956e18519ef7fbbaf8f";
-  config.params['language'] = "en-US";
+  config.params['api_key'] = '3b9c855ca7a0d956e18519ef7fbbaf8f';
+  config.params['language'] = 'en-US';
   return config;
 });
 
@@ -15,30 +15,34 @@ export const moviesApi = {
   nowPlaying: () => api.get('movie/now_playing'),
   upcoming: () => api.get('movie/upcoming'),
   popular: () => api.get('movie/popular'),
-  movieDetail: id => api.get(`movie/${id}`, {
-    params: {
-      append_to_response: 'videos'
-    }
-  }),
-  search: term => api.get('search/movie', {
-    params: {
-      query: encodeURIComponent(term)
-    }
-  })
+  movieDetail: (id) =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: 'videos',
+      },
+    }),
+  search: (term) =>
+    api.get('search/movie', {
+      params: {
+        query: encodeURIComponent(term),
+      },
+    }),
 };
 
 export const tvApi = {
   topRated: () => api.get('tv/top_rated'),
   popular: () => api.get('tv/popular'),
-  airingToday: () => api.get('tv/airingToday'),
-  showDetail: id => api.get(`tv/${id}`, {
-    params: {
-      append_to_response: 'videos'
-    }
-  }),
-  search: term => api.get('search/tv', {
-    params: {
-      query: encodeURIComponent(term)
-    }
-  })
+  airingToday: () => api.get('tv/airing_today'),
+  showDetail: (id) =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_response: 'videos',
+      },
+    }),
+  search: (term) =>
+    api.get('search/tv', {
+      params: {
+        query: encodeURIComponent(term),
+      },
+    }),
 };
