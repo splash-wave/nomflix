b258d938d8302792e0b0112d818e2eb187d11a94
arimkim <arim721@gmail.com>
2020-04-13 17:48:49 +0900

    SearchContainer

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -4,17 +4,20 @@ import Home from 'Routes/Home';
 import TV from 'Routes/TV';
 import Search from 'Routes/Search';
 import Header from 'Components/Header';
+import Detail from 'Components/Detail';
 
 export default () => (
   <Router>
     <>
-    <Header />
-    <Switch>
-      <Route path='/' exact component={Home} />
-      <Route path='/tv' exact component={TV} />
-      <Route path='/search' component={Search} />
-      <Redirect from='*' to='/' />
-    </Switch>
+      <Header />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/tv' exact component={TV} />
+        <Route path='/search' component={Search} />
+        <Route path='/movie/:id' component={Detail} />
+        <Route path='/show/:id' component={Detail} />
+        <Redirect from='*' to='/' />
+      </Switch>
     </>
   </Router>
 );
--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchPresenter from './SearchPresenter';
+import { moviesApi, tvApi } from 'api';
 
 export default class extends React.Component {
   state = {
@@ -10,8 +11,45 @@ export default class extends React.Component {
     loading: false,
   };
 
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== '') {
+      this.searchByTerm(searchTerm);
+    }
+  };
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    this.setState({
+      loading: true,
+    });
+    try {
+      const {
+        data: { results: movieResults },
+      } = await moviesApi.search(searchTerm);
+      const {
+        data: { results: tvResults },
+      } = await tvApi.search(searchTerm);
+      console.log(movieResults, tvResults);
+    } catch {
+      this.setState({ error: 'Can`t find results.' });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
   render() {
     const { movieResults, tvResults, searchTerm, error, loading } = this.state;
-    return <SearchPresenter movieResults={movieResults} tvResults={tvResults} searchTerm={searchTerm} error={error} loading={loading} />;
+    console.log(this.state);
+    return (
+      <SearchPresenter
+        movieResults={movieResults}
+        tvResults={tvResults}
+        searchTerm={searchTerm}
+        error={error}
+        loading={loading}
+        handleSubmit={this.handleSubmit}
+      />
+    );
   }
 }
